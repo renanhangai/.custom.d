28c4702a725af3e78f2c6cfedd91bde2402306d4
renanhangai <hangai.renan@gmail.com>
2017-02-25 21:01:04 -0300

    Updated install script

--- install.sh
+++ install.sh
@@ -2,7 +2,23 @@
 
 CUSTOM_DIR=${CUSTOM_DIR:-"$HOME/.custom.d"}
 
-mkdir -p $CUSTOM_DIR
-wget -O - https://github.com/renanhangai/.config.d/archive/master.tar.gz | tar -xzf - --strip-components=1 -C "$CUSTOM_DIR"
-sh "$CUSTOM_DIR/setup.sh"
+if [ -d "$CUSTOM_DIR" ]; then
+	echo "$CUSTOM_DIR already exists. Please delete it or remove";
+	exit;
+fi
+
+if [ -n "$(type -t git)" ]; then
+	git clone https://github.com/renanhangai/.config.d "$CUSTOM_DIR"
+else
+	echo "Downloading file";
+	mkdir -p $CUSTOM_DIR
+	wget -qO - https://github.com/renanhangai/.config.d/archive/master.tar.gz | tar -xzf - --strip-components=1 -C "$CUSTOM_DIR"
+fi
+
+
+
+ 
+#mkdir -p $CUSTOM_DIR
+#wget -O - https://github.com/renanhangai/.config.d/archive/master.tar.gz | tar -xzf - --strip-components=1 -C "$CUSTOM_DIR"
+#sh "$CUSTOM_DIR/setup.sh"
 
