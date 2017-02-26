c3b4d1a40ec403e2c65a505d5d828e96dbd64b49
renanhangai <hangai.renan@gmail.com>
2017-02-25 21:02:59 -0300

    Fixed missing setup.sh on the end of .custom.d

--- install.sh
+++ install.sh
@@ -8,17 +8,13 @@ if [ -d "$CUSTOM_DIR" ]; then
 fi
 
 if [ -n "$(type -t git)" ]; then
-	git clone https://github.com/renanhangai/.config.d "$CUSTOM_DIR"
+	git clone https://github.com/renanhangai/.config.d "$CUSTOM_DIR";
 else
 	echo "Downloading file";
-	mkdir -p $CUSTOM_DIR
-	wget -qO - https://github.com/renanhangai/.config.d/archive/master.tar.gz | tar -xzf - --strip-components=1 -C "$CUSTOM_DIR"
+	mkdir -p $CUSTOM_DIR;
+	wget -qO - https://github.com/renanhangai/.config.d/archive/master.tar.gz | tar -xzf - --strip-components=1 -C "$CUSTOM_DIR";
 fi
 
-
-
- 
-#mkdir -p $CUSTOM_DIR
-#wget -O - https://github.com/renanhangai/.config.d/archive/master.tar.gz | tar -xzf - --strip-components=1 -C "$CUSTOM_DIR"
-#sh "$CUSTOM_DIR/setup.sh"
+echo "\n\n\n\nSetupping .custom.d";
+sh "$CUSTOM_DIR/setup.sh";
 
