2c82f31e36d1e8f461d7c28ac13ffdeb6459efcf
renanhangai <hangai.renan@gmail.com>
2017-02-26 00:11:32 -0300

    Added bash setup

--- setup.sh
+++ setup.sh
@@ -28,7 +28,34 @@ emacsSetup() {
 	fi
 }
 
+bashSetup() {
+	if [ ! -f "$HOME/.bashrc" ]; then
+		echo ".bashrc NOT found"
+		return
+	fi
+
+	echo "Setupping .bashrc ..."
+	if cat "$HOME/.bashrc" | grep -qEz "\#:auto\.custom\.d:.*?\#:~auto\.custom\.d:"; then
+		echo ".custom.d already setupped on .bashrc"
+		return
+	fi
+	BASHRC_CONTENT="
+
+#:auto.custom.d: 
+if [ -f \"$CUSTOM_DIR/.bashrc\" ]; then
+\t# CUSTOM_PS1_LOCAL='\u@\h'
+\t# CUSTOM_PS1_COLOR='32m'
+\t. \"$CUSTOM_DIR/.bashrc\"
+fi
+#:~auto.custom.d:"
+
+	echo "$BASHRC_CONTENT" >> "$HOME/.bashrc"
+	return
+}
+
 emacsSetup
+echo ""
+bashSetup
+echo ""
 
-
-
+echo "Successfully setupped everything"
