fd0e00d447ee50eb5b23e4dac2193abd8b762a85
renanhangai <hangai.renan@gmail.com>
2017-03-11 12:53:36 -0300

    Added curl option for download

--- install.sh
+++ install.sh
@@ -9,10 +9,17 @@ fi
 
 if [ -n "$(type -t git)" ]; then
 	git clone https://github.com/renanhangai/.custom.d "$CUSTOM_DIR";
-else
+elif [ -n "$(type -t curl)" ]; then
+	echo "Downloading file";
+	mkdir -p $CUSTOM_DIR;
+    curl -L https://github.com/renanhangai/.custom.d/archive/master.tar.gz | tar -xzf - --strip-components=1 -C "$CUSTOM_DIR";
+elif [ -n "$(type -t wget)" ]; then
 	echo "Downloading file";
 	mkdir -p $CUSTOM_DIR;
 	wget -qO - https://github.com/renanhangai/.custom.d/archive/master.tar.gz | tar -xzf - --strip-components=1 -C "$CUSTOM_DIR";
+else
+	echo "Cannot download. Please install curl or wget."
+	exit;
 fi
 
 echo "\n\n\n\nSetupping .custom.d";
